99eef3e29b291790f161ead90183eb5366db0975
Roshan Devkota <rd@skylink-technologies.de>
2025-06-01 07:09:18 +0200

    changing central domain

--- api/config/tenancy.php
+++ api/config/tenancy.php
@@ -18,7 +18,7 @@ return [
      * Only relevant if you're using the domain or subdomain identification middleware.
      */
     'central_domains' => [
-        'back.myapp',
+        'back.roshandevkota.com.np',
         // 'localhost',
         // '127.0.0.1',
     ],
